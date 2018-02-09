b41f634e2388ebcd871a38cd425f9a8e71ea184d
heeyeonwiz <heeyeon.hwang@gmail.com>
2018-02-09 21:37:46 +0900

    Update image.php

--- image.php
+++ image.php
@@ -4,9 +4,10 @@ $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
+$red = imagecolorallocate($im, 255, 0, 0);
 
 // Draw a white rectangle
-imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+imagefilledrectangle($im, 0, 0, 199, 199, $red);
 //imagefilledrectangle($im, 0, 0, 199, 199, $green);
 
 // Save the image
